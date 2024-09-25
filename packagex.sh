91922bfdd492e11a4ef79a42e8519e751f2db178
Rob Dickinson <robfromboulder@gmail.com>
2024-09-25 01:28:35 -0600

    Add provenance and sbom metadata

--- packagex.sh
+++ packagex.sh
@@ -4,4 +4,4 @@ docker rm supertanker
 docker volume rm supertanker
 docker image rm -f supertanker:6.1.0
 docker system prune -f
-docker buildx build --platform linux/amd64,linux/arm64 --build-arg VERSION=$1 -f container.dockerfile -t robfromboulder/supertanker:$1 --no-cache --push .
+docker buildx build --platform linux/amd64,linux/arm64 --provenance=true --sbom=true --build-arg VERSION=$1 -f container.dockerfile -t robfromboulder/supertanker:$1 --no-cache --push .
